695df0507678341a4543b9e25921c2e893e2b0d7
Grant <gjennings@ecomsystems.com>
2020-12-11 08:30:32 -0500

    chore: moved logo to static

--- sites/petersons/components/AppBar.vue
+++ sites/petersons/components/AppBar.vue
@@ -12,7 +12,7 @@
     <v-container fill-height>
       <nuxt-link to="/">
         <v-img
-          :src="require('~/assets/logo.png')"
+          :src="require('/images/logo.png?nf_resize=fit&w=285')"
           contain
           max-width="285"
         ></v-img>
