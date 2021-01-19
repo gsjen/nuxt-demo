b5d62d533c5a622d2f16b8c32f2c5ed0cb651c32
Grant <gjennings@ecomsystems.com>
2021-01-19 14:57:01 -0500

    updated paths

--- media/bin/build-media.js
+++ media/bin/build-media.js
@@ -11,21 +11,24 @@ const glob = require('glob')
   }
 
   process.chdir('..')
-  glob('!(node_modules)/**/static/**/*.@(png|jpg|pdf)', async (er, files) => {
+  glob('content/*/media/**/*.@(png|jpg|jpeg|pdf)', async (er, files) => {
     if (er) {
       consola.fatal(er)
       return
     }
     for (file of files) {
       const parts = path.parse(file)
-      const dirs = parts.dir.split('/').reduce((r, v, i) => {
-        if (i > 0 && v !== 'static') {
-          r.push(v)
+      const dirs = parts.dir.split('/')
+      const dest = dirs.reduce((r, v, i) => {
+        if (i !== 0 && i !== 2) {
+          if (!(i === 1 && v === '$shared')) {
+            r.push(v)
+          }
         }
         return r
       }, [])
 
-      const dir = path.join('media/dist', ...dirs)
+      const dir = path.join('media/dist', ...dest)
       await fs.mkdir(dir, { recursive: true })
 
       try {
